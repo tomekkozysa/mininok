60664707515c35c585bc4ab152611fcaec9aec81
Tomasz Kozysa <tomek@15nine.com>
2020-04-21 08:29:39 +0100

    gatsby cms settings update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
+// import Blog from "../components/blog"
 
 class IndexPage extends React.Component {
   render() {
     const siteTitle = "Home"
+    const { data } = this.props
+    const posts = data.allMdx.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -18,7 +21,36 @@ class IndexPage extends React.Component {
         <h1>
           H1 title
         </h1>
+
         <p>Body copy.</p>
+      
+      
+        <div>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3>
+                  <Link
+                    to={`blog${node.fields.slug}`}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+
+
+        
+        {/* <Blog /> */}
         
       </Layout>
     )
@@ -26,3 +58,28 @@ class IndexPage extends React.Component {
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+  `
